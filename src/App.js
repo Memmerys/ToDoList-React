073f86230adc6e3c2c83e048b89e1f41efc7737d
Helle Emmery <emmery94@gmail.com>
2019-02-11 13:43:30 +0100

    react todoList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Todos from './Components/Todos';
+import AddTodo from './Components/AddTodo';
+import About from './Components/pages/About';
+import Header from './Components/Layout/Header';
+//import uuid from 'uuid';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    todos: []
+  }
+componentDidMount() {
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10').then(res => this.setState({ todos: res.data }))
+}
+
+
+  // Toggle Complete
+markComplete = (id) => {
+ this.setState({ todos: this.state.todos.map(todo => {
+    if(todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo;
+ }) });
+}
+
+//Delete Todo
+delTodo = (id) => {
+  axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }))
+}
+
+//Add todo
+addTodo = (title) => {
+  axios.post('https://jsonplaceholder.typicode.com/todos', {
+    title,
+    completed: false
+  })
+    .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+}
+
+
   render() {
     return (
+     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container"> 
+        <Header />
+        <Route exact path="/" render={props => (
+          <React.Fragment>
+             <AddTodo addTodo={this.addTodo} />
+            <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+          </React.Fragment>
+        )} />
+        <Route path="/about" component={About} />
+        </div> 
       </div>
+     </Router>
     );
   }
 }
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header style={headerStyle}>
         <h1>TodoList</h1>
+        <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
     </header>
   )
 }
@@ -13,6 +15,11 @@ const headerStyle = {
     color: '#eddee1',
     textAlign: 'center',
     padding: '10px'
+}
+
+const linkStyle = {
+  color: '#eddee1',
+  textDecoration: 'none'
 }
 
 export default Header;
